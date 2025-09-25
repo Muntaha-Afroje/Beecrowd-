e1b90397eaf2b90c2a35738a8e21958628972129
Muntaha-Afroje <muntahaafroje@gmail.com>
2025-09-25 12:59:14 +0600

    Add files via upload

---
+++ Beecrowd1038.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    float price,Total;
+    scanf("%d %d",&X,&Y);
+    switch(X)
+    {
+    case 1:
+        price=4.00;
+        break;
+    case 2:
+        price=4.50;
+        break;
+    case 3:
+        price=5.00;
+        break;
+    case 4:
+        price=2.00;
+        break;
+    case 5:
+        price=1.50;
+        break;
+    default:
+        price=0.0;
+        break;
+    }
+    Total=Y*price;
+    printf("Total: R$ %.2f\n",Total);
+    return 0;
+}
